d332fb0f721cbe55651bbb3dd60d4d3b1f6044fb
Vu Tuan Anh <vu.tuan.anh@framgia.com>
2016-08-10 14:49:21 +0700

    ref Task#3_BoardActivity

---
+++ app/src/main/java/com/example/framgia/carobluetooth/data/Constants.java
@@ -0,0 +1,14 @@
+package com.example.framgia.carobluetooth.data;
+
+/**
+ * Created by framgia on 10/08/2016.
+ */
+public class Constants {
+    public static final int MARGIN = 8;
+    public static final int CELL_SIZE = 80;
+    public static final int STROKE_WIDTH = 2;
+    public static final int COL = 30;
+    public static final int ROW = 30;
+    public static final int BOARD_WIDTH = 2400;
+    public static final int BOARD_HEIGHT = 2400;
+}
